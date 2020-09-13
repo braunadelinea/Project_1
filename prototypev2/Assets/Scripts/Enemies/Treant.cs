865a75dd6bdf2522b330fe363adebafb8b9ba4a7
Toby Klauder <tobyklauder@hotmail.com>
2020-09-12 18:51:54 -0700

    treant

---
+++ prototypev2/Assets/Scripts/Enemies/Treant.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treant : MonoBehaviour
+{
+    private GameObject currentroom;
+    private GameObject player;
+    public enum Phase { Idle, Attacking }
+    private Phase currentPhase = Phase.Idle;
+    private float phaseStartTime;
+
+    private float idleLength = 2f;
+
+    private float idleTimer = 0;
+    private Animator animator;
+    public GameObject grenade;
+    private Vector3 movementDirection;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("player");
+        phaseStartTime = Time.time;
+        animator = gameObject.GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Detected Trigger Collision " + collision.gameObject.name);
+        if (currentroom != null)
+        {
+            return;
+        }
+        else
+        {
+            currentroom = collision.gameObject;
+            Debug.Log("Room not found for entity " + gameObject.name + " assigning to current detected.");
+        }
+    }
+    private void Update()
+    {
+        if (currentPhase == Phase.Idle)
+        {
+            idleTimer += Time.deltaTime;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (currentroom == GameObject.FindGameObjectWithTag("player").GetComponent<PlayerManager>().currentroom && GameObject.Find("Spawner").GetComponent<spawnplayer>().GetSpawnedPlayer())
+        {
+            UpdatePhase();
+            ApplyPhase();
+            ChangeSpriteDirection();
+        }
+    }
+    private void UpdatePhase()
+    {
+        Phase previousphase = currentPhase;
+        switch (currentPhase)
+        {
+            case Phase.Idle:
+                if (idleTimer > idleLength)
+                {
+                    currentPhase = Phase.Attacking;
+                    idleTimer = 0;
+                    animator.SetTrigger("Start Attack");
+                }
+                break;
+            case Phase.Attacking:
+                currentPhase = Phase.Idle;
+                animator.SetTrigger("Start Idle");
+                break;
+        }
+        if (previousphase != currentPhase)
+        {
+            phaseStartTime = Time.time;
+            Debug.Log("Phase changed from " + previousphase + " to " + currentPhase);
+        }
+    }
+    private void ApplyPhase()
+    {
+        switch (currentPhase)
+        {
+            case Phase.Idle:
+                ChangeMoveDir(Vector3.zero);
+                return;
+            case Phase.Attacking:
+                int grenadeXOffset = Random.Range(1, 5);
+                int grenadeYOffset = Random.Range(1, 5);
+                int operation = Random.Range(0, 4);
+                switch (operation) {
+                    case 0: 
+                        Instantiate(grenade, new Vector3(this.transform.position.x + grenadeXOffset, this.transform.position.y + grenadeYOffset, this.transform.position.z), Quaternion.identity);
+                        break;
+                    case 1:
+                        Instantiate(grenade, new Vector3(this.transform.position.x + grenadeXOffset, this.transform.position.y - grenadeYOffset, this.transform.position.z), Quaternion.identity);
+                        break;
+                    case 2:
+                        Instantiate(grenade, new Vector3(this.transform.position.x - grenadeXOffset, this.transform.position.y + grenadeYOffset, this.transform.position.z), Quaternion.identity);
+                        break;
+                    case 3:
+                        Instantiate(grenade, new Vector3(this.transform.position.x - grenadeXOffset, this.transform.position.y - grenadeYOffset, this.transform.position.z), Quaternion.identity);
+                        break; 
+                }
+                idleTimer = 0;
+                currentPhase = Phase.Idle;
+                break;
+        }
+    }
+    private void ChangeSpriteDirection()
+    {
+        if (movementDirection.x > 0)
+        {
+            Vector3 temp = transform.localScale;
+            temp.x = 1;
+            gameObject.transform.localScale = temp;
+        }
+        else if (movementDirection.x < 0)
+        {
+            Vector3 temp = transform.localScale;
+            temp.x = -1;
+            gameObject.transform.localScale = temp;
+        }
+        if (currentPhase == Phase.Attacking)
+        {
+            if (gameObject.transform.position.x - player.transform.position.x > 0)
+            {
+                Vector3 temp = transform.localScale;
+                temp.x = -1;
+                gameObject.transform.localScale = temp;
+            }
+            else
+            {
+                Vector3 temp = transform.localScale;
+                temp.x = 1;
+                gameObject.transform.localScale = temp;
+            }
+        }
+    }
+    private void ChangeMoveDir(Vector2 newDirection)
+    {
+        movementDirection = newDirection;
+    }
+}
---
+++ prototypev2/Assets/Scripts/Enemies/TreantGrenade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreantGrenade : MonoBehaviour
+{
+    private float explosionTimer;
+    private float growthTimer; 
+    private float explosionRadius = 2;
+    private GameObject indicator; 
+    private void Update()
+    {
+        Vector3 scaleChange = new Vector3(0.8f, 0.8f, 0);
+        explosionTimer += Time.deltaTime;
+        if (growthTimer > 0.25) {
+            indicator.transform.localScale += scaleChange;
+            growthTimer = 0; 
+        }
+        if (explosionTimer > 1.5f) {
+            Collider2D[] collider = Physics2D.OverlapCircleAll(this.transform.position, explosionRadius);
+            for (int i = 0; i < collider.Length; i++) {
+                if (collider[i].gameObject.tag == "player") {
+                    collider[i].gameObject.GetComponent<PlayerManager>().DecreaseHealth(2);
+                }
+            }
+            Destroy(this.gameObject);
+            Destroy(indicator.gameObject); 
+        }
+    }
+}
